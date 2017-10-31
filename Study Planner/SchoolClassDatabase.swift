962471ab866c807c19f2b205353d3dbe1a801f57
LaurenWhite <lauren5826@gmail.com>
2017-10-31 00:57:25 -0400

    Created Settings tab, grouped files

---
+++ Study Planner/SchoolClassDatabase.swift
@@ -0,0 +1,9 @@
+//
+//  SchoolClassDatabase.swift
+//  Study Planner
+//
+//  Created by Lauren White on 10/31/17.
+//  Copyright © 2017 Lauren White. All rights reserved.
+//
+
+import Foundation
---
+++ Study Planner/SettingsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SettingsViewController.swift
+//  Study Planner
+//
+//  Created by Lauren White on 10/31/17.
+//  Copyright © 2017 Lauren White. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var classNameTextField: UITextField!
+    
+    @IBAction func classNameEntered(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
