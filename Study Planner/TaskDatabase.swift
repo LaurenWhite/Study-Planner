ab32f488d047bb5d43cf94c126558be35e236385
LaurenWhite <lauren5826@gmail.com>
2017-11-02 17:25:02 -0400

    Implementing data persistence

--- Study Planner/TaskDatabase.swift
+++ Study Planner/TaskDatabase.swift
@@ -8,17 +8,20 @@
 
 import Foundation
 private var tasks: [Task] = []
+let taskKey = "tasks.key"
+
 
 class TaskDatabase {
     
     
-    /*init() { // 3
+    init() { // 3
         // Create some data to work with initially
-        var currentIndex: Int = tasks.index(after: (tasks.count))
+        /*var currentIndex: Int = tasks.index(after: (tasks.count))
         saveNew(task: Task(taskTitle: String(currentIndex) + ". Read Assigned", completion: false, dueDate: Date()))
         currentIndex = tasks.index(after: (tasks.count))
-        saveNew(task: Task(taskTitle: String(currentIndex) + ". Study for Calc Test", completion: true, dueDate: Date()))
-    }*/
+        saveNew(task: Task(taskTitle: String(currentIndex) + ". Study for Calc Test", completion: true, dueDate: Date()))*/
+        tasks = loadSavedTasks()
+    }
     
     //Returns: The number of notes in the database
     var countTasks: Int {
@@ -38,5 +41,31 @@ class TaskDatabase {
     
     private func reorder(){
         tasks.sort(by: { $0.dueDate < $1.dueDate })
+        saveTasks(tasks: tasks)
+    }
+    
+    func saveTasks(tasks: [Task]) {
+        var data: [[String:Any]] = [] // 1
+        for task in tasks {
+            let taskData: [String:Any] = ["taskTitle" : task.taskTitle, "completion" : task.completion, "dueDate" : task.dueDate] // 2
+            data.append(taskData)
+        }
+        UserDefaults.standard.set(data, forKey: taskKey) // 3
+    }
+    
+    func loadSavedTasks() -> [Task] {
+        // 1
+        let savedData = UserDefaults.standard.array(forKey: taskKey) as? [[String:AnyObject]] ?? []
+        var array: [Task] = []
+        for taskData in savedData {
+            // 2
+            if let taskTitle = taskData["taskTitle"] as? String,
+                let completion = taskData["completion"] as? Bool,
+                let dueDate = taskData["dueDate"] as? Date {
+                // 3
+                array.append(Task(taskTitle: taskTitle, completion: completion, dueDate: dueDate))
+            }
+        }
+        return array
     }
 }
