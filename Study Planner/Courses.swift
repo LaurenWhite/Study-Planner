437854898c60fbe7fb02b318125f2a2328f65ca0
LaurenWhite <lauren5826@gmail.com>
2017-11-02 22:03:47 -0400

    Created file/classes for the courses

---
+++ Study Planner/Courses.swift
@@ -0,0 +1,33 @@
+//
+//  Courses.swift
+//  Study Planner
+//
+//  Created by Lauren White on 11/2/17.
+//  Copyright © 2017 Lauren White. All rights reserved.
+//
+
+import Foundation
+
+var courses: [Course] = []//change later for persistence
+
+class Course{
+    var courseTitle: String
+    var events: Array<Any>
+    
+    init(courseTitle: String, events: Array<Any>){
+        self.courseTitle = courseTitle
+        self.events = []
+    }
+}
+
+class CourseDatabase{
+    
+    //Returns: The number of notes in the database
+    func countCourses() -> Int {
+        return courses.count
+    }
+    
+    func addCourse(course: Course){
+        courses.append(course)
+    }
+}
--- Study Planner/SchoolClassDatabase.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SchoolClassDatabase.swift
-//  Study Planner
-//
-//  Created by Lauren White on 10/31/17.
-//  Copyright © 2017 Lauren White. All rights reserved.
-//
-
-import Foundation
